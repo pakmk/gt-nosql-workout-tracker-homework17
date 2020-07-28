5b6d6546a4765553e7de10dda2d98d7b87f99ee5
min <pakmk8@gmail.com>
2020-07-27 22:00:05 -0400

    updated apiRoutes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const views = require("./routes/viewRoutes.js");
+const viewRoutes = require("./routes/viewRoutes.js");
+const apiRoutes = require("./routes/apiRoutes.js")
 
 const PORT = process.env.PORT || 3030;
 
@@ -16,7 +17,8 @@ mongoose.connect("mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-app.use(views);
+app.use(viewRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
