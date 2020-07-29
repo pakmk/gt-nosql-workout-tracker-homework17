b68dd4ef490e92466fa83df2ed8acabaa88e3c0f
min <pakmk8@gmail.com>
2020-07-28 22:33:45 -0400

    added virtuals and updated readMe

--- models/workout.js
+++ models/workout.js
@@ -2,14 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema ({
-
+const workoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now
     },
     exercises: Array
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
-const Workout = mongoose.model("Workout", workoutSchema)
+const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
